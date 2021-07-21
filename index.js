b4dfa5a081e24c94a492d5dfdd03e7999fd7598a
thomasmckenna <thomasmckenna12@gmail.com>
2021-07-20 21:52:15 -0400

    /Users/thomasmckenna/phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Code your solutions in this file
+// If you know how many times you are going to run your loop then the for loop is the way to go. 
+
+
+//     const gifts = ["teddy bear", "drone", "doll"];
+//     
+//     function wrapGifts(gifts) {
+//       for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//       }
+//     
+//       return gifts;
+//     }
+//     
+//     wrapGifts(gifts); 
+//     
+//     
+//     function writeCards(cards)
+
+
+
+ // for (let i = 0; i < letters.length; i++) {
+ // console.log(letters[i]); 
+
+
+    
+    
+ /// let j = 0 
+ ///  
+ ///  while(j < letters.length) {
+ ///      console.log(letters[j]);
+ ///      j++; 
+ ///  }
+
+
+
+function writeCards(names, event) {
+    let messages = []; 
+    for(let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`); 
+    }
+    return messages; 
+}
+
+function countDown(positiveNumber) {
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber); 
+        positiveNumber--; 
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
